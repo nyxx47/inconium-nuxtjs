fc8e9f08f5a8273dbfbbf453808cd880ab01b3fb
krisna ahroid <ahroidlife@gmail.com>
2019-07-14 10:04:44 +0700

    add meta

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,22 @@ import Initial from '~/components/Initial.vue';
 import Landing from '~/components/Landing.vue';
 
 export default {
+  head() { // <-- property used by vue-meta to add header tags
+        return {
+          htmlAttrs: {
+            lang: 'en'
+          },
+          title: 'INCONIUM - Software HUB', // <-- For our title tag
+          meta: [
+            {
+              hid: 'description',  
+              name: 'description', // <-- for our meta description tag
+              content:
+                'INCONIUM is the central of a software engineer, UI/UX Designer and QA Freelancer that focuses on software development.'
+            }
+          ]
+        }
+  },
   data(){
     return{
       isVisible: true
